946d43366911ffeee4c99c55a27a87cded802ff8
LukeGent <lucgent@gmail.com>
2018-04-27 15:25:45 +0200

    DB

--- LucaWebApi/LucaWebApi/Controllers/HomeController.cs
+++ LucaWebApi/LucaWebApi/Controllers/HomeController.cs
@@ -3,12 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LucaWebApi.Models;
 
 namespace LucaWebApi.Controllers {
 	public class HomeController : Controller {
 		public ActionResult Index() {
 			ViewBag.Title = "Home Page";
 
+			return View();
+		}
+
+		public ActionResult About() {
+			ViewBag.Message = "Your application description page.";
+
+			return View();
+		}
+
+		public ActionResult Contact() {
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+		public ActionResult RicercaProdotti() {
+			ViewBag.Messagge = "Benvenuto nella pagina di ricerca prodotto!";
+			return View();
+		}
+		public ActionResult RicercaProdotti(string codice, string descrizione) {
+			int cod;
+			DomainModels dm = new DomainModels();
+			if (int.TryParse(codice, out cod)) {
+				ViewBag.prodotto = dm.RicercaId(cod);
+				return View("DettagliOProdotto");
+			}
+
+			return View();
+		}
+
+		public ActionResult DettaglioProdotto() {
+			ViewBag.Messagge = " ECCO I DETTAGLI DEL TUO PRODOTTO!";
+			return View();
+		}
+
+		public ActionResult DettaglioProdotto(int id) {
+			DomainModels dm = new DomainModels();
+			ViewBag.Messagge = " ECCO I DETTAGLI DEL TUO PRODOTTO!";
+			ViewBag.prodotto = dm.RicercaId(id);
+			return View();
+		}
+		public ActionResult PreviewRichiesta() {
+			return View();
+		}
+		public ActionResult ElencoProdotti() {
+			return View();
+		}
+		public ActionResult AggiungiCarrello(string codice, string Quantita) {
 			return View();
 		}
 	}
---
+++ LucaWebApi/LucaWebApi/Models/DomainModels.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LucaWebApi.Models {
+	public class Prodotto{
+		public int ID { get; set; }
+		public string Descrizione { get; set; }
+		public int Quantita { get; set; }
+	}
+
+	public interface IRichieste{
+		Prodotto RicercaId(int id);
+		List<Prodotto> RicercaDescrizione(string descrizione);
+		void AggiungiOrdine(List<Prodotto> listP);
+	}
+
+	public class DomainModels: IRichieste {
+		public Prodotto RicercaId(int id){
+			throw new NotImplementedException();
+		}
+
+		public List<Prodotto> RicercaDescrizione(string descrizione) {
+			throw new NotImplementedException();
+		}
+
+		public void AggiungiOrdine(List<Prodotto> listP) {
+			throw new NotImplementedException();
+		}
+
+	}
+}
